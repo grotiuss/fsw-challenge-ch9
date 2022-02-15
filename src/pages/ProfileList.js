0a347e40e659d41533b8eb5df7569c9256b09b8c
meddhawi <meddhawi@gmail.com>
2022-02-15 21:22:16 +0700

    added profile list

---
+++ src/pages/ProfileList.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Card, Col, Row, Container, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+import firebase from '../auth/firebase';
+
+class ProfileList extends Component{
+    constructor(props){
+        super(props);
+    
+        this.state ={
+            isLoading:true,
+        };
+    }
+
+    componentDidMount(){
+        this.getData()
+    }
+
+    async getData(){
+        try {
+            const snapshot = await firebase.database().ref('profile').once('value')
+            this.setState({
+                isLoading: false,
+                value: snapshot.val()
+            })
+        const { value } = this.state;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    get listOfUsers(){
+        const { value } = this.state;
+        return(
+            <div>
+                <div className='row text-center'>
+                {Object.keys(value).map(function(name, index){
+                        return (
+                                <Card style={{ width: '18rem' }} key={index} className='m-3'>
+                                    <Card.Img variant="top" 
+                                     src={value[name].imageLink}/>
+                                     <Card.Title>{value[name].name}</Card.Title>
+                                    <Card.Body>
+                                    <ListGroup className="list-group-flush">
+                                        <ListGroupItem>Description: {value[name].description}</ListGroupItem>
+                                        <ListGroupItem>Level: {value[name].point}</ListGroupItem>
+                                    </ListGroup>
+                                    </Card.Body>
+                                </Card>
+                        )
+                })}
+                </div>
+            </div>
+        )
+    }
+
+    Loader(){
+        return <h3>Loading...</h3>
+
+    }
+
+    render(){
+        return(
+            <div>
+                <Container>
+
+                    {this.state.isLoading ? this.Loader : this.listOfUsers}
+
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default ProfileList;
