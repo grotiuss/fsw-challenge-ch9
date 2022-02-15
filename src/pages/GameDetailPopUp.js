b735ed24a90f8635f700207d40a6da7b25df4261
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-15 18:35:26 +0700

    Adding leaderboard

--- src/pages/GameDetailPopUp.js
+++ src/pages/GameDetailPopUp.js
@@ -22,20 +22,18 @@ function GameDetailPopUp(props) {
             <div className="popup-inner">
                 <CloseModalButton className='close-btn' onClick={() => props.setTrigger(false)}></CloseModalButton>
                 <Row>
-                    <Col className='game-detail-img col-md-5'></Col>
-                    <Col className="popup-content col-md-7 col-12">
-                        <h3>{props.data.name}</h3>
-                        <p>{props.data.description}</p>
-                        <a className='btn btn-success' href={props.data.route}>Play!</a>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
+                    <Col className='game-detail-img col-sm-5 col-md-5'></Col>
+                    <Col className="popup-content col-sm-7 col-md-7 col-12">
+                        <Row>
+                            <Col className="col-12">
+                                <h3><b>{props.data.name}</b></h3>
+                                <p>{props.data.description}</p>
+                                <a className='btn btn-success' href={props.data.route}>Play!</a>
+                            </Col>
+                            <Col className="col-12 mt-5 text-end pe-5">
+                                <h4><b>Leaderboard</b></h4>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </div>
